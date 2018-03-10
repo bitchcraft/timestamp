e9af595b9bb4fe8a351b0186f25f8d0fe8306d91
Josh Li <josh@maddrag0n.local>
2018-03-10 14:28:54 +0100

    feat(Timestamp) iso timestamp converter with tests

---
+++ src/Timestamp.js
@@ -0,0 +1,61 @@
+// @flow
+import Timezone, { fromNumber, toNumberOptions } from './Timezone';
+
+/**
+ * Pads numbers < 10 with a leading 0
+ *
+ * @private
+ * @param  {Number} number - number to pad
+ * @return {String}        - padded number
+ */
+const pad = (number: number): string => String(number).padStart(2, '0');
+
+
+/**
+ * @public
+ * @module Timestamp
+ */
+const Timestamp = {
+	fromDate: fromDate,
+};
+
+/**
+ *
+ * Outputs RFC3339 / ISO8601 String from Date
+ *
+ *
+ * @public
+ * @func timestamp
+ * @memberof Timestamp
+ * @param {Date}   [date=new Date()] - Date object
+ * @param {Timezone} [timezone]      - Optional timezone to shift the time to (effective UTC time will remain the same).
+ */
+export function fromDate(date: Date = new Date(), timezone: ?Timezone) {
+	const offset = timezone instanceof Timezone
+		? timezone.toNumber(toNumberOptions.MILLISECONDS)
+		: 0;
+
+	date = new Date(
+		// get time as ms UTC
+		date.getTime()
+		// adjust for local time offset
+		+ (date.getTimezoneOffset() * 60 * 1000)
+		// adjust for target TZ
+		+ offset
+	);
+
+	const tz = timezone instanceof Timezone ? timezone.value : fromNumber(date.getTimezoneOffset(), true).value;
+	const year = date.getFullYear();
+	const month = pad(date.getMonth() + 1);
+	const day = pad(date.getDate());
+	const hours = pad(date.getHours());
+	const minutes = pad(date.getMinutes());
+	const seconds = pad(date.getSeconds());
+	const milliseconds = (date.getMilliseconds() / 1000).toFixed(3).slice(2, 5);
+
+	return `${year}-${month}-${day}`
+		+ `T${hours}:${minutes}:${seconds}.${milliseconds}`
+		+ `${tz}`;
+}
+
+export default Timestamp.fromDate;
---
+++ src/Timestamp.test.js
@@ -0,0 +1,32 @@
+/* eslint-env jest */
+import timestampFromDate, { fromDate } from './Timestamp';
+import Timezone from './Timezone';
+
+describe('Timestamp', () => {
+	it('should export { fromDate }', () => expect(typeof fromDate).toBe('function'));
+	it('should default export fromDate', () => expect(typeof timestampFromDate).toBe('function'));
+
+	const io = [{
+		in: [
+			new Date('December 17, 1995 03:24:00 +0000'),
+			new Timezone('Z'),
+		],
+		out: '1995-12-17T03:24:00.000Z',
+	}, {
+		in: [
+			new Date('December 17, 1995 03:24:00 +0000'),
+			new Timezone('-0400'),
+		],
+		out: '1995-12-16T23:24:00.000-0400',
+	}, {
+		in: [
+			new Date('December 17, 1995 03:24:00 +0000'),
+			new Timezone('+0100'),
+		],
+		out: '1995-12-17T04:24:00.000+0100',
+	}];
+
+	io.forEach((test, i) => {
+		it(`fromDate(...test[${i}]) should return '${test.out}'`, () => expect(fromDate(...test.in)).toBe(test.out));
+	});
+});
